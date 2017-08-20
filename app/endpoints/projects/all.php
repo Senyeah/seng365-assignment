8534d0ab5ba2815b12714710746764e1a91fe339
Jack Greenhill <jackgreenhill@me.com>
2017-08-20 19:48:12 +1200

    Trigger CI update

--- app/endpoints/projects/all.php
+++ app/endpoints/projects/all.php
@@ -4,6 +4,10 @@ require_once "../../engine/runtime.php";
 
 class RetrieveAllProjects implements APIEngine\Requestable {
 
+    /**
+     * @method GET
+     * @route /projects
+     */
 	public function execute($request) {
 		phpinfo();
 	}
